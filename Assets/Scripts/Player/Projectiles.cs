cf1c32ef0cf4e8c6a4464152fee441af05b21e6d
Gr4yskull <augusto.bacilieri@gmail.com>
2026-02-25 17:54:03 +0100

    added bullet shooter

--- Assets/Scripts/Player/Projectiles.cs
+++ Assets/Scripts/Player/Projectiles.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class Projectiles : MonoBehaviour
 {
     Rigidbody rb;
+    float speed=300f;
 
     private void Start()
     {
         rb=GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward*speed*Time.deltaTime,ForceMode.Impulse);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/Turrets.cs
+++ Assets/Scripts/Player/Turrets.cs
@@ -2,7 +2,20 @@ using UnityEngine;
 
 public class Turrets : MonoBehaviour
 {
+    [SerializeField] GameObject bullet;
     [SerializeField] float bulletRate;
     [SerializeField] float damage;
     [SerializeField] int cost;
+    float timer;
+
+    private void Update()
+    {
+        timer+=Time.deltaTime;
+        if (timer >= bulletRate)
+        {
+            Instantiate(bullet,transform.position,transform.rotation,transform);
+            timer=0;
+        }
+        
+    }
 }
