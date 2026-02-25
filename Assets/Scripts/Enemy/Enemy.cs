5b89af2db5939f74fe4cd846724b6e4632831f70
Gr4yskull <augusto.bacilieri@gmail.com>
2026-02-25 12:07:34 +0100

    Created Enemy stuff
    -created enemy prefabs
    -created despawn enemy
    -created enemy movement
    -created enemy spawner

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health;
+    [SerializeField] float damagePerHit;
+    [SerializeField] float speed;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        //get rigidbody
+        rb=GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.left*speed,ForceMode.Impulse);
+    }
+
+}
